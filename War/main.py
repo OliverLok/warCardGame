095550e4694be6153b71eb315b2af52f3315807c
OliverLok <oliver.lokhandwala@gmail.com>
2022-02-08 18:49:43 -0800

    fixed 3 issue

--- War/main.py
+++ War/main.py
@@ -1,18 +1,25 @@
+import random
+
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11,
          'Queen':12, 'King':13, 'Ace':14}
-
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 class Card:
     def __init__(self, suit, rank):
-        Card.suit = suit
-        Card.rank = rank
-        Card.value = values[rank]
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
 
     def __str__(self):
         return Card.rank + " of " + Card.suit
 
+class Deck:
+    def __init__(self):
+        self.allCards = []
+
+        for suit
+
 twoHearts = Card('Hearts', 'Two')
 threeClubs = Card('Clubs','Three')
-
-print(twoHearts.value)
 
 print(twoHearts.value < threeClubs.value)
